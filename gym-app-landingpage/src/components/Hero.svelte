21fcd674da27d9db0969dd7f8c61231d54cf53b8
mark <markpackham1@gmail.com>
2024-02-13 10:09:05 +0000

    Add h2 for Hero
    https://youtu.be/vb7CgDcA_6U?feature=shared&t=1605

--- gym-app-landingpage/src/components/Hero.svelte
+++ gym-app-landingpage/src/components/Hero.svelte
@@ -3,5 +3,12 @@
   import SectionWrapper from "./SectionWrapper.svelte";
 </script>
 <SectionWrapper>
-<Header />
+  <Header />
+  <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+    <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center">
+      <span class="text-indigo-400">
+        Gym
+      </span>
+    </h2>
+  </div>
 </SectionWrapper>

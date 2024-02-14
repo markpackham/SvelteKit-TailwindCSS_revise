a29ad7b0f3e6b67e550fee54a822e28d3c32f8ef
mark <markpackham1@gmail.com>
2024-02-14 18:21:25 +0000

    Add content to ProductCard
    https://youtu.be/vb7CgDcA_6U?si=r5jHAhA1SGpDaGaJ&t=2430

--- gym-app-landingpage/src/components/Product.svelte
+++ gym-app-landingpage/src/components/Product.svelte
@@ -1,6 +1,7 @@
 <script>
   import ProductCard from "./ProductCard.svelte";
 import SectionWrapper from "./SectionWrapper.svelte";
+
   let productFeatures = [{
       featureList: [
         "Daily streak counter",
@@ -33,6 +34,7 @@ import SectionWrapper from "./SectionWrapper.svelte";
     },
   ];
 </script>
+
 <SectionWrapper id="product">
   <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
     <div class="flex flex-col gap-2">
@@ -43,10 +45,8 @@ import SectionWrapper from "./SectionWrapper.svelte";
         Features For Everyone
       </h3>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 relative text-base">
       {#each productFeatures as productFeature}
         <ProductCard />
       {/each}
-    </div>
   </div>
 </SectionWrapper>
---
+++ gym-app-landingpage/src/components/ProductCard.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let index;
+    export let productFeature;
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 relative text-base">
+<div class="flex flex-col gap-8 md:gap-10">
+    <h4
+    class="text-2xl sm:text-3xl md:text-4xl max-w-[1000px] w-full font-medium relative pr-10 after:absolute after:top-full after:left-0 after:w-1/5 after:h-1.5 after:mt-1 after:bg-slate-900"
+>
+    <slot />
+</h4>
+<p>{productFeature.description}</p>
+<div class="flex flex-col gap-3"></div>
+</div>
+</div>

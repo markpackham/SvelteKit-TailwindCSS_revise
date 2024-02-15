7996863ef3fc2902d7d5cc55fe2de92d41851118
mark <markpackham1@gmail.com>
2024-02-15 16:35:45 +0000

    Setup reRoute function
    https://youtu.be/vb7CgDcA_6U?si=4NKhirT30YLx6WeZ&t=4871

--- gym-app-landingpage/src/components/Footer.svelte
+++ gym-app-landingpage/src/components/Footer.svelte
@@ -1,3 +1,9 @@
+<script>
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+  </script>
+  
+
 <footer class="py-16 sm:py-20 md:py-24 px-4 md:px-8">
     <div
         class="max-w-[1200px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base"
@@ -6,7 +12,7 @@
             <h1 class="font-semibold">
                 Swoley <span class="text-indigo-400">Moley</span>
             </h1>
-            <p>©2023 SWOLEY MOLEY LTD. All rights reserved.</p>
+            <p>©{currentYear} SWOLEY MOLEY LTD. All rights reserved.</p>
         </div>
         <div class="flex flex-col gap-4">
             <p class="font-bold poppins text-base sm:text-lg">Support</p>
@@ -43,9 +49,17 @@
                 target="_blank"
                 class="cursor-pointer hover:text-indigo-400 duration-200"
             >
-                <i class="fa-brands fa-youtube pr-2" />
-                YouTube</a
+                <i class="fa-brands fa-odysee pr-2" />
+                Odysee</a
             >
+            <a
+            href=""
+            target="_blank"
+            class="cursor-pointer hover:text-indigo-400 duration-200"
+        >
+            <i class="fa-brands fa-youtube pr-2" />
+            YouTube</a
+        >
             <a
                 href=""
                 target="_blank"
--- gym-app-landingpage/src/routes/+layout.svelte
+++ gym-app-landingpage/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
   import "../app.css";
   import Footer from "../components/Footer.svelte";
+
+  let y;
+  $: outerHeight = 0;
 </script>
 
 <slot />
 <Footer />
+<svelte:window bind:scrollY={y} bind:outerHeight/>
